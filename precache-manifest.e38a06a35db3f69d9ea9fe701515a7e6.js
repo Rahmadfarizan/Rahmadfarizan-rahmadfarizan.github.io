309050ff9018288e95e6c4f8582fdc429049d986
Rahmad Farizan <57408301+Rahmadfarizan@users.noreply.github.com>
2022-03-23 14:42:52 +0700

    Add files via upload

---
+++ precache-manifest.e38a06a35db3f69d9ea9fe701515a7e6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf720ed10bc97892afff55bae7f013f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4fbde97b3cccdbfbbea",
+    "url": "/static/css/main.c5b33b42.chunk.css"
+  },
+  {
+    "revision": "b0e41f9453b5a00efe29",
+    "url": "/static/js/2.0e965160.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.0e965160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4fbde97b3cccdbfbbea",
+    "url": "/static/js/main.7ebe1fbd.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "7f91c7f1ae79092a69dcf9eb0acc862f",
+    "url": "/static/media/diskominfo.7f91c7f1.png"
+  },
+  {
+    "revision": "029989562a56a7902ee17c0359ce2171",
+    "url": "/static/media/ganeshaoperation.02998956.png"
+  },
+  {
+    "revision": "7b09005a36c4c837e4ac2b23e588e75f",
+    "url": "/static/media/profile.7b09005a.jpeg"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  }
+]);
